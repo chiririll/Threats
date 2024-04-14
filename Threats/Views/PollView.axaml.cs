2c65fadd264f53b5ac000124c7227da009d92a3a
Kirill Chizhov <sstive39@gmail.com>
2024-04-15 02:57:27 +0300

    [WIP] Создание View по туториалу

---
+++ Threats/Views/PollView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Threats.Views;
+
+public partial class PollView : UserControl
+{
+    public PollView()
+    {
+        InitializeComponent();
+    }
+}
