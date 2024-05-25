e3f78f9528622cac6b5bcf3ce2cc888f2bdfe24f
Kirill Chizhov <sstive39@gmail.com>
2024-05-26 01:56:06 +0300

    Попытка в strings.axaml

--- Threats/App.axaml.cs
+++ Threats/App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 using Threats.ViewModels;
 using Threats.Views;
 
@@ -11,6 +13,8 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        InitStrings();
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -24,5 +28,16 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void InitStrings()
+    {
+        if (!Current!.TryGetResource("strings", ThemeVariant.Default, out var stringsResource)
+            || stringsResource is not IResourceProvider strings)
+        {
+            return;
+        }
+
+        Current.Resources.MergedDictionaries.Add(strings);
     }
 }
